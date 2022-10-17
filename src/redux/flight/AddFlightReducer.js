7c51032c189a19f6a0cf692cd18c8e5c2768299d
abiodunraheem <raheemabiodun23@gmail.com>
2022-10-18 00:07:07 +0100

    Create flight action

--- src/redux/flight/AddFlightReducer.js
+++ src/redux/flight/AddFlightReducer.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
 
-const CREATE_FLIGHT = VIRGIN-ATLANTA-FRONTEND/CREATE_FLIGHT;
+const CREATE_FLIGHT = 'VIRGIN-ATLANTA-FRONTEND/CREATE_FLIGHT';
 
-const createFlight = (payload) => {
-    type: CREATE_FLIGHT,
-    payload
-}
+const createFlight = (payload) => ({
+  type: CREATE_FLIGHT,
+  payload,
+});
 
-const addFlight = (payload) => async(dispatch) => {
-    axios.post('http://127.0.0.1:3000/api/v1/flights/', payload)
-    .then((res)=> {
-        dispatch(createFlight(res.data))
+export const addFlight = (payload) => async (dispatch) => {
+  axios.post('http://127.0.0.1:3000/api/v1/flights/', payload)
+    .then((res) => {
+      dispatch(createFlight(res.data));
     })
-    .catch((error)=> {
-        dispatch(createFlight(error.response.data))
-    })
-}
+    .catch((error) => {
+      dispatch(createFlight(error.response.data));
+    });
+};
 
 const flightState = [];
 const addFlightReducer = (state = flightState, action) => {
-    switch (action.type) {
-        case CREATE_FLIGHT:
-            return [...state, action.payload];
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case CREATE_FLIGHT:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
 
 export default addFlightReducer;
--- src/redux/flight/FlightListReducer.js
+++ src/redux/flight/FlightListReducer.js
@@ -1,29 +1,29 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const fetchFlights = createAsyncThunk('fetchFlights', async()=> {
-    const response = await axios.get('http://127.0.0.1:3000/api/v1/flights/');
-    const flights = await response.data;
-    return flights;
+const fetchFlights = createAsyncThunk('fetchFlights', async () => {
+  const response = await axios.get('http://127.0.0.1:3000/api/v1/flights/');
+  const flights = await response.data;
+  return flights;
 });
 
 const flightSlice = createSlice({
-    name: 'flights',
-    initialState: {
-        flight: '',
-        loading: false,
-        error: false
+  name: 'flights',
+  initialState: {
+    flight: '',
+    loading: false,
+    error: false,
+  },
+  reducer: {},
+  extraReducer: {
+    [fetchFlights.fulfilled]: (state, action) => {
+      const newState = {
+        ...state,
+        flight: action.payload,
+      };
+      return newState;
     },
-    reducer: {},
-    extraReducer: {
-        [fetchFlights.fulfilled]: (state, action) => {
-            const newState = {
-                ...state,
-                flight: action.payload
-            }
-            return newState;
-        }
-    }
+  },
 });
 
 const flightReducer = flightSlice.reducer;
