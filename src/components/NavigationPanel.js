24ca03e31d9dae69ae3cf1419571f39363db7051
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-04 17:52:03 +0100

    Add Links to navigation file

---
+++ src/components/NavigationPanel.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdClose } from 'react-icons/io';
+import { FaBars } from 'react-icons/fa';
+
+const NavigationPanel = () => {
+  const [NavIsActive, toggleActiveNav] = useState(false);
+  const isActive = true;
+
+  const handleSignOut = () => {
+    sessionStorage.removeItem('loginToken');
+    window.reload();
+  };
+  return (
+    <aside>
+      <button
+        type="button"
+        className="mobile-toggle btn"
+        onClick={() => {
+          toggleActiveNav(!NavIsActive);
+        }}
+      >
+        {NavIsActive ? (
+          <FaBars />
+
+        ) : (
+          <IoMdClose />
+        )}
+      </button>
+      <div className="header__logo">
+        <Link to="/home">
+          {' '}
+          {/* <img src={logo} alt="logo" /> */}
+        </Link>
+      </div>
+      <ul>
+        <li className={isActive ? 'active-link' : ''}>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/home">Home</Link>
+        </li>
+        <li className={isActive ? 'active-link' : ''}>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/add_flight">Add Flight</Link>
+        </li>
+        <li className={isActive ? 'active-link' : ''}>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/delete_flight">Delete Flight</Link>
+        </li>
+        <li className={isActive ? 'active-link' : ''}>
+          <Link onClick={() => toggleActiveNav(!NavIsActive)} to="/reservation">Reservation</Link>
+        </li>
+        <li className={isActive ? 'active-link' : ''}>
+          <Link onClick={handleSignOut} to="/">Sign Out</Link>
+        </li>
+      </ul>
+      <ul className="social-links">
+        <li>
+          <span>
+            <i className="fa-brands fa-linkedin-in" />
+          </span>
+        </li>
+        <li>
+          <span>
+            <i className="fa-brands fa-facebook" />
+          </span>
+        </li>
+        <li>
+          <span>
+            <i className="fa-brands fa-twitter" />
+          </span>
+        </li>
+        <li>
+          <span>
+            <i className="fa-brands fa-whatsapp" />
+          </span>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default NavigationPanel;
