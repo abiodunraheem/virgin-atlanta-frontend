c493649a907ba483dff13ac048b97617cdd427b9
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-04 22:34:36 +0100

    Add all dependencies and update App.css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavigationPanel from './components/NavigationPanel';
+import Home from './components/Home';
+import DeleteFlight from './components/DeleteFlight';
+import Reservations from './components/Reservations';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import AddFlight from './components/AddFlight';
+import AddReservation from './components/AddReservation';
+import FlightDetails from './components/FlightDetails';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome To Virgin Atlanta</h1>
+    <div className="app">
+      {(sessionStorage.getItem('loginToken')) ? <NavigationPanel /> : null }
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/flight/:id" element={<FlightDetails />} />
+        <Route path="/delete_flight" element={<DeleteFlight />} />
+        <Route path="/addFlight" element={<AddFlight />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/addReservation/:id" element={<AddReservation />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/ConfigureStore';
 import './index.css';
@@ -9,9 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
 );
 
