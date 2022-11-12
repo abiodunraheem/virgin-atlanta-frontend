b60d33685d9817b8f4f1b016f0cadb73cc754626
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-12 23:06:57 +0100

    Add flight component

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Flight = () => (
-  <h2>Flight</h2>
+const Flight = ({
+  id, avatar, flightName, price,
+}) => (
+  <div className="card mb-3 details-container">
+    <Link key={id} to={`/airline/${id}`} className="link">
+      <div className="img-container">
+        <img src={avatar} alt={flightName} />
+      </div>
+      <div className="cancelled-flights contents">
+        <h3>{flightName}</h3>
+        {' '}
+        ................
+        <h4>
+          Price: $
+          {price}
+        </h4>
+      </div>
+    </Link>
+  </div>
 );
+
+Flight.propTypes = {
+  id: PropTypes.number,
+  avatar: PropTypes.string,
+  flightName: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Flight.defaultProps = {
+  id: null,
+  avatar: null,
+  flightName: null,
+  price: null,
+};
 
 export default Flight;
