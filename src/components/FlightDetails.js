77fc8c341c3fd59ad17ac6434e999a22855b7f0d
abiodunraheem <raheemabiodun23@gmail.com>
2022-11-12 23:30:11 +0100

    Add flight details component

--- src/components/FlightDetails.js
+++ src/components/FlightDetails.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
-const FlightDetails = () => (
-  <h2>FlightDetails</h2>
-);
+const FlightDetails = () => {
+  const flight = useParams();
+  const dataList = useSelector((state) => state.flights.flights.data);
+  const flightDetails = dataList.filter((item) => +item.id === +flight.id);
+
+  return (
+    <div className="container my-5">
+      {flightDetails.map((item) => (
+        <div className="details-container" key={item.id}>
+          <div className="img-container">
+            <img src={item.image} alt={item.name} />
+          </div>
+          <div className="contents p-5">
+            <div className="p-3">
+              <p>Name: </p>
+              <h3>{item.name}</h3>
+            </div>
+
+            <div className="p-3 my-2">
+              <p>Price: $</p>
+              {' '}
+              <h4>{item.price}</h4>
+            </div>
+            <Link className="btn book-btn my-2" to={`/reservationForm/${item.id}`}>Book now</Link>
+            <Link className="btn my-2" to="/home">Back</Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default FlightDetails;
